e886017acc47f8eaeea22af8a9162415c9fba3fe
Bryan <bryankhansen@gmail.com>
2022-10-05 22:48:57 -0600

    M2 final

--- src/main/java/com/pluralsight/singleton/DbSingleton.java
+++ src/main/java/com/pluralsight/singleton/DbSingleton.java
@@ -1,14 +1,31 @@
 package com.pluralsight.singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
+
+    private Connection conn = null;
 
     private static class LazyHolder {
         static final DbSingleton INSTANCE = new DbSingleton();
     }
 
-    private DbSingleton () {}
+    private DbSingleton () {
+        try {
+            String jdbcURL = "jdbc:h2:mem:test";
+            conn = DriverManager.getConnection(jdbcURL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static DbSingleton getInstance() {
         return LazyHolder.INSTANCE;
+    }
+
+    public Connection getConnection() {
+        return conn;
     }
 }
--- src/main/java/com/pluralsight/singleton/DbSingletonDemo.java
+++ src/main/java/com/pluralsight/singleton/DbSingletonDemo.java
@@ -1,16 +1,30 @@
 package com.pluralsight.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbSingletonDemo {
 
-    public static void main (String [] args) {
+    public static void main (String [] args) throws SQLException {
         DbSingleton instance = DbSingleton.getInstance();
 
         System.out.println(instance);
 
-        DbSingleton anotherInstance = DbSingleton.getInstance();
+        Connection conn = instance.getConnection();
 
-        System.out.println(anotherInstance);
+        Statement statement = conn.createStatement();
 
+        statement.execute("Create table students (ID int primary key, name varchar(65))");
+
+        System.out.println("Create table students.");
+
+        int rows = statement.executeUpdate("Insert into students (ID, name) values (1, 'Bryan')");
+
+        if(rows > 0) {
+            System.out.println("Inserted a new row.");
+        }
+         conn.close();
     }
 
 }
